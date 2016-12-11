70b814c23dd6a31adc19a612f00f7d5a787f29ff
Gordon <gordon@gordonmurray.com>
2016-12-11 19:34:38 +0000

    check for event

--- public/webhooks.php
+++ public/webhooks.php
@@ -12,19 +12,21 @@ $data = file_get_contents("php://input");
 // parse webhook data in to an array
 $webHookData = $teamwork->receivePostedData($data);
 
+if(isset($webHookData['event']) && $webHookData['event']!='') {
 // call different events depending on the webhook
-switch ($webHookData['event']) {
-    case "TASK.CREATED":
-        // Get the Task, Prepare a new Description, Update the Task
-        $task = $teamwork->get('tasks', $webHookData['objectId']);
-        $taskDescription = $teamwork->prepareTaskDescription($task);
-        $response = $teamwork->put('tasks', $webHookData['objectId'], $taskDescription);
-        echo $response;
-        break;
-    case "TASK.COMPLETED":
-        $task = $teamwork->get('task', $webHookData['objectId']);
-        break;
-    default:
-        echo 'No data posted';
-        break;
+    switch ($webHookData['event']) {
+        case "TASK.CREATED":
+            // Get the Task, Prepare a new Description, Update the Task
+            $task = $teamwork->get('tasks', $webHookData['objectId']);
+            $taskDescription = $teamwork->prepareTaskDescription($task);
+            $response = $teamwork->put('tasks', $webHookData['objectId'], $taskDescription);
+            echo $response;
+            break;
+        case "TASK.COMPLETED":
+            $task = $teamwork->get('task', $webHookData['objectId']);
+            break;
+        default:
+            echo 'No data posted';
+            break;
+    }
 }

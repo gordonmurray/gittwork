aeba08292b1021f0a692df6fbd93cc120712ed06
Gordon <gordon@gordonmurray.com>
2017-01-01 22:37:07 +0000

    fixed wrong json being used

--- public/events/task_created.php
+++ public/events/task_created.php
@@ -8,7 +8,7 @@ $task = $teamwork->get('tasks', $webHookData['objectId']);
 // prepare a Description
 $taskDescription = $teamwork->prepareTaskDescription($task);
 
-$taskJson = json_encode(array('todo-item' => array('description' => $data)));
+$taskJson = json_encode(array('todo-item' => array('description' => $taskDescription)));
 
 // Update the Task
 $response = $teamwork->put('tasks', $webHookData['objectId'], $taskJson);
